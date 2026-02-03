6eadaa016e7a41cc78ac1158dc18a24dd8cee5a5
Skand Tripathi <skandtripathi7@gmail.com>
2026-02-03 17:11:23 +0530

    one three

--- 3_5/one.py
+++ 3_5/one.py
@@ -1,7 +1,29 @@
-a = 25
-b= 5
+nums = []
 
-c=a +b
-
-print(c)
-
+def analyze_numbers(nums):
+	count= 0
+	total = 0
+	maxi  = None
+	
+	for num in nums:
+		if isinstance(num,(int,float)):
+			count += 1
+			total += num
+			if maxi == None or num > maxi:
+				maxi = num
+			
+	
+	average = total / count if count>0 else None
+		
+	ana_dict = {
+				"count":count, 
+				"sum":total,
+				"average": average,
+				"max": maxi,
+		
+		}
+		
+	return ana_dict
+		
+		
+print(analyze_numbers(nums))
